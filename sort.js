b419a770f96de7ed0417c82661563fe87c8f7e06
byungju0624 <byungju0624@gmail.com>
2021-04-12 12:37:57 +0900

    selectSort add

--- sort.js
+++ sort.js
@@ -13,7 +13,7 @@ class ArrayList {
       array[index1] = array[index2];
       array[index2] = aux;
     };
-
+    //버블 정렬
     this.bubbleSort = () => {
       let length = array.length;
       for (let i = 0; i < length; i++) {
@@ -32,6 +32,23 @@ class ArrayList {
           if (array[j] > array[j + 1]) {
             swap(j, j + 1);
           }
+        }
+      }
+    };
+    //삽입 정렬
+
+    this.slectSort = () => {
+      let length = array.length,
+        indexMin;
+      for (let i = 0; i < length - 1; i++) {
+        indexMin = i;
+        for (let j = i; j < length; j++) {
+          if (array[indexMin] > array[j]) {
+            indexMin = j;
+          }
+        }
+        if (i !== indexMin) {
+          swap(i, indexMin);
         }
       }
     };
