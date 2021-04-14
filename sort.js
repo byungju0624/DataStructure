994032b9513ec35a2b679ef78bf91915c3f82a9a
byungju0624 <byungju0624@gmail.com>
2021-04-14 11:24:31 +0900

    mergeSort study

--- sort.js
+++ sort.js
@@ -69,5 +69,40 @@ class ArrayList {
         array[j] = temp;
       }
     };
+
+    //병합 정렬
+    const merge = (left, right) => {
+      let result = [],
+        il = 0,
+        ir = 0;
+      while (il < left.length && ir < right < length) {
+        if (left[il] < right[ir]) {
+          result.push(left[il++]);
+        } else {
+          result.push(right[ir++]);
+        }
+      }
+      while (il < left.length) {
+        result.push(left[il++]);
+      }
+      while (ir < right.length) {
+        result.push(right[ir++]);
+      }
+      return result;
+    };
+    const mergeSortRec = (array) => {
+      let length = array.length;
+      if (length === 1) {
+        return array;
+      }
+      let mid = Math.floor(length / 2),
+        left = array.slice(0, mid),
+        right = array.slice(mid, length);
+
+      return merge(mergeSortRec(left), mergeSortRec(right));
+    };
+    this.mergeSort = () => {
+      array = mergeSortRec(array);
+    };
   }
 }
